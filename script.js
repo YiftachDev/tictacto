7ee4346fc95fcbc8e4854b76204372147cf48958
YiftachDev <yiftach007@gmail.com>
2023-06-10 20:36:01 +0300

    Created game module

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-const gameBoard = (function () {
-  let _gameBoard = ["x", "x", "x", "x", "x", "x", "x", "x", "x"];
-  function render() {
-    for (let i = 0; i < _gameBoard.length; i++) {
-      let currentField = document.querySelector(`[data-index="${i}"]`);
-      currentField.innerHTML = _gameBoard[i];
+const game = function () {
+  const gameBoard = (function () {
+    let _gameBoard = ["", "", "", "", "", "", "", "", ""];
+    function render() {
+      for (let i = 0; i < _gameBoard.length; i++) {
+        let currentField = document.querySelector(`[data-index="${i}"]`);
+        currentField.innerHTML = _gameBoard[i];
+      }
     }
-  }
-  return { render };
-})();
+    return { render };
+  })();
 
-function Player(name, sign) {
-  return { name, sign };
-}
+  function Player(name, sign) {
+    return { name, sign };
+  }
+};
