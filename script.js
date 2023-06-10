ddacd2047f61692437a008562620b823df7bad99
YiftachDev <yiftach007@gmail.com>
2023-06-10 21:31:58 +0300

    Created handleClick funtion and updateBoard

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const game = function () {
+const game = (function () {
   const gameBoard = (function () {
     let _gameBoard = ["", "", "", "", "", "", "", "", ""];
     function render() {
@@ -7,10 +7,33 @@ const game = function () {
         currentField.innerHTML = _gameBoard[i];
       }
     }
-    return { render };
+    function updateBoard(index, mark) {
+      _gameBoard[index] = mark;
+      render();
+    }
+    return { render, updateBoard };
   })();
 
-  function Player(name, sign) {
-    return { name, sign };
+  function Player(name, mark, turn) {
+    return { name, mark, turn };
   }
-};
+
+  let player1 = new Player("a", "x", true);
+  let player2 = new Player("b", "O", false);
+
+  function handleClick() {
+    for (let i = 0; i < 9; i++) {
+      let field = document.querySelector(`[data-index="${i}"]`);
+      field.addEventListener("click", () => {
+        if (player1.turn)
+          gameBoard.updateBoard(field.getAttribute("data-index"), "x");
+        else gameBoard.updateBoard(field.getAttribute("data-index"), "O");
+        player1.turn = !player1.turn;
+        player2.turn = !player2.turn;
+      });
+    }
+  }
+  return { handleClick };
+})();
+
+game.handleClick();
