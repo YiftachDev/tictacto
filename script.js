a95693ab7d976ca8e143b2a3bfd8b679c774c42a
YiftachDev <yiftach007@gmail.com>
2023-06-10 20:16:26 +0300

    Created render function to render the gameboard

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const gameBoard = (function () {
-  let _gameBoard = [
-    [1, 2, 3],
-    [1, 2, 3],
-    [1, 2, 3],
-  ];
-  function render() {}
+  let _gameBoard = ["x", "x", "x", "x", "x", "x", "x", "x", "x"];
+  function render() {
+    for (let i = 0; i < _gameBoard.length; i++) {
+      let currentField = document.querySelector(`[data-index="${i}"]`);
+      currentField.innerHTML = _gameBoard[i];
+    }
+  }
+  return { render };
 })();
