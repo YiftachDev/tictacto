8423143f826ebc8310fcc7399c32adb8dc9ff032
YiftachDev <yiftach007@gmail.com>
2023-06-11 18:57:06 +0300

    Created checkWin function

--- script.js
+++ script.js
@@ -9,22 +9,21 @@ const game = (function () {
     }
     function updateBoard(index, mark) {
       if (_gameBoard[index] != "") {
-        alert("Not avalible");
         return;
       }
       _gameBoard[index] = mark;
       render();
       changeTurns();
     }
-    return { render, updateBoard };
+    return { updateBoard };
   })();
 
   function Player(name, mark, turn) {
     return { name, mark, turn };
   }
 
-  let player1 = new Player("a", "x", true);
-  let player2 = new Player("b", "O", false);
+  let player1 = Player("a", "x", true);
+  let player2 = Player("b", "O", false);
 
   function handleClick() {
     for (let i = 0; i < 9; i++) {
@@ -33,7 +32,45 @@ const game = (function () {
         if (player1.turn)
           gameBoard.updateBoard(field.getAttribute("data-index"), "x");
         else gameBoard.updateBoard(field.getAttribute("data-index"), "O");
+        checkWin();
       });
+    }
+  }
+
+  const _winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  function checkWin() {
+    for (let winCondition = 0; winCondition < _winConditions.length; i++) {
+      if (
+        document.querySelector(
+          `[data-index="${winCondition}"]`[0].textContent ==
+            document.querySelector(`[data-index="${winCondition}"]`)[1]
+        ) &&
+        document.querySelector(`[data-index="${winCondition}"]`)[1] ==
+          document.querySelector(`[data-index="${winCondition}"]`)[2]
+      ) {
+        if (
+          document.querySelector(`[data-index="${winCondition}"]`)
+            .textContent == "x"
+        ) {
+          printWinner(player1.name);
+          return;
+        } else {
+          printWinner(player2.name);
+          return;
+        }
+      } else {
+        printTie();
+        return;
+      }
     }
   }
 
